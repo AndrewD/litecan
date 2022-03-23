013e7c593019e0147a2b4c2f7d847d740a31c6f5
Andrew Dennison <andrew.dennison@motec.com.au>
2022-03-23 11:53:15 +1100

    setup: simplify with wildcard

--- setup.py
+++ setup.py
@@ -2,24 +2,10 @@
 # manual setup: python3 setup.py develop --user
 
 from setuptools import setup
-from setuptools import find_packages
-import os
-
-
-extra_files = ["rtl_lst.txt"]
-
-cdir = os.path.dirname(__file__)
-mdir = os.path.abspath(os.path.join(cdir, 'ctucanfd'))
-sdir = os.path.abspath(os.path.join(mdir, 'ctucanfd_ip_core'))
-
-with open(os.path.join(mdir, 'rtl_lst.txt')) as f:
-    for line in f:
-        srcfile = os.path.join(sdir, line.strip().replace('rtl', 'src'))
-        extra_files.append(srcfile)
 
 setup(
     name="ctucanfd",
     packages=["ctucanfd"],
     include_package_data=True,
-    package_data={"": extra_files},
+    package_data={"": ["rtl_lst.txt", "ctucanfd/ctucanfd_ip_core/src/**/*.vhd"] },
 )
